64e7be44fa344fa626ef3671248e13feae729a0e
Jorge Barragan <jmarq19@outlook.com>
2021-09-28 15:58:28 -0700

    updating with the ability to log scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//these variables correspond to elements on my page.
 var startButton = document.getElementById('startButton');
 var starterText = document.getElementById('starterText');
 var questionTextEl = document.getElementById('questionText');
@@ -11,15 +12,20 @@ var scorecardEl = document.getElementById('scorecard');
 var finalScoreEl = document.getElementById('finalScoreText');
 var logButtonEl = document.getElementById('logButton');
 var usersInitialsArea = document.getElementById("userInitials");
+var highScoresContainerEl = document.getElementById('highScoresContainer')
+var highScoresEl = document.getElementById("highScores");
 
+//these global variables are needed by multiple functions
 var rightAnswer;
 var correctAnswers;
 var userAnswer;
 var secondsLeft;
 var questionIndex = 0;
+var timerInterval;
+var userLog = [];
 
 
-//this is an array of question data formatted in JSON.
+//this is an array of question objects.
 let questionSet = [
     {
         "questionText": "This is a sample question",
@@ -44,20 +50,19 @@ let questionSet = [
 function countdown() {
     timeEl.textContent = "Time left: " + secondsLeft + " seconds.";
     // Sets interval in variable
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = "Time left: " + secondsLeft + " seconds.";
   
-      if(secondsLeft === 0) {
+      if(secondsLeft <= 0) {
         // Stops execution of action at set interval
-        endQuiz();
-        clearInterval(timerInterval);        
+        endQuiz();        
       }
   
     }, 1000);
 }
 
-
+//this function places my question and response text on the page
 function askAQuestion(){
 
     var theQuestionText = questionSet[questionIndex].questionText;
@@ -77,6 +82,7 @@ function askAQuestion(){
 
 }
 
+//this function evaluates the user's response to see if they answered wrong or right.
 function checkAnswer(){
         if (userAnswer === rightAnswer){
             correctAnswers++;
@@ -104,8 +110,10 @@ function checkAnswer(){
         }
 }
 
+//This event listener corresponds to the button that starts the quiz.
 startButton.addEventListener("click", startQuiz);
 
+//begins the quiz for the user.
 function startQuiz(){ 
 
     secondsLeft = 15;
@@ -115,9 +123,11 @@ function startQuiz(){
     starterText.classList.add("hidden");
     gameContentEl.classList.remove("hidden");
 
-    finalScoreEl.classList.add("hidden");
+    highScoresContainerEl.classList.add("hidden");
     logButtonEl.classList.add("hidden");
     usersInitialsArea.classList.add("hidden");
+    finalScoreEl.classList.add("hidden");
+    
     
   
     //begins the countdown timer.
@@ -127,12 +137,13 @@ function startQuiz(){
 
 }
 
-function displayHighScores(){}
 
+//this function ends the quiz.
 function endQuiz(){
+    clearInterval(timerInterval);
     questionIndex = 0;
 
-    var finalScoreText = "Your final socre was: " + correctAnswers + " right answers!";
+    var finalScoreText = "Your final score was: " + correctAnswers + " right answers!";
     starterText.classList.remove("hidden");
     startButton.classList.remove("hidden");
     gameContentEl.classList.add("hidden");
@@ -141,9 +152,10 @@ function endQuiz(){
     finalScoreEl.classList.remove("hidden");
     logButtonEl.classList.remove("hidden");
     usersInitialsArea.classList.remove("hidden");
+    
 }
 
-
+//these event listeners correspond to the A,B,C,D answer buttons during the quiz.
 optionA.addEventListener("click", function(){
     userAnswer = questionSet[questionIndex].optionA;
     checkAnswer();
@@ -164,13 +176,36 @@ optionD.addEventListener("click", function(){
     checkAnswer();    
 });
 
+//this button helps the user log their score and their initials in the local storage. 
 logButtonEl.addEventListener("click", function(){
     var userInitials = usersInitialsArea.value;
+    console.log(userInitials);
+
+
+
+    userLog = JSON.parse(localStorage.getItem("userScores")) || [];
+
     if(userInitials != null){
-        var userLog = {initials:userInitials, score:correctAnswers};
-    window.localStorage.setItem(JSON.stringify(userLog));
+        userLog.push({initials:userInitials, score:correctAnswers});
+    window.localStorage.setItem("userScores", JSON.stringify(userLog));
     }
+    displayHighScores();
 })
+
+//this function displays the data that was logged in the local storage.
+function displayHighScores(){
+    logButtonEl.classList.add("hidden");
+    usersInitialsArea.classList.add("hidden");
+    highScoresContainerEl.classList.remove("hidden");
+
+    userLog = JSON.parse(localStorage.getItem("userScores")) || [];
+    for(var i = 0; i < userLog.length; i++){
+        var listItem = document.createElement("li");
+        listItem.innerText = userLog[i].initials + " : " + userLog[i].score;
+        highScoresEl.append(listItem);
+    }
+
+}
 
 
 
